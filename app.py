a1e070dc235f25f6baced6d142387c425123d0ff
Cyprien G <cyprien.guillemot@gmail.com>
2016-05-28 12:38:12 +0200

    BUG Various fix

--- app.py
+++ app.py
@@ -30,15 +30,16 @@ def connect():
 
 @socketio.on('disconnect')
 def disconnect():
-    print('Client disconnected')
+    log.info('Client disconnected')
 
 
 @socketio.on('load map')
 def load_map(tweet_id):
     poly = determinate_tweet_location(tweet_id)
+    log.info(u'Finished to determine tweet {0} location'.format(tweet_id))
     emit('map ready', {'poly': poly, 'maps_key': os.environ['GOOGLE_MAPS_KEY']})
 
 
 if __name__ == "__main__":
     log.info(u'Starting server on port: {0}'.format(int(sys.argv[1])))
-    socketio.run(app,  port=int(sys.argv[1]))
+    socketio.run(app, port=int(sys.argv[1]))

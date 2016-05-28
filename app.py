c530950b1ff56aae172a9fed83ba589a8b5fb635
Cyprien G <cyprien.guillemot@gmail.com>
2016-05-28 18:31:48 +0200

    Revert "ENH: Use websockets to show the map"
    This reverts commit e485e2683079ec10803284d8c914eda6cced4732.

--- app.py
+++ app.py
@@ -1,20 +1,12 @@
 import os
-import sys
 
 from flask import Flask
 from flask import render_template
-from flask.ext.socketio import emit
-from flask.ext.socketio import SocketIO
+from flask import request
 
 from lib.tweet_locator import determinate_tweet_location
 
-import logging
-log = logging.getLogger('werkzeug')
-log.setLevel(logging.DEBUG)
-
 app = Flask(__name__)
-
-socketio = SocketIO(app)
 
 
 @app.route('/')
@@ -23,23 +15,13 @@ def home():
     return render_template('home.html')
 
 
-@socketio.on('connect')
-def connect():
-    emit('response', {'data': 'Connected'})
-
-
-@socketio.on('disconnect')
-def disconnect():
-    log.info('Client disconnected')
-
-
-@socketio.on('load map')
-def load_map(tweet_id):
-    poly = determinate_tweet_location(tweet_id)
-    log.info(u'Finished to determine tweet {0} location'.format(tweet_id))
-    emit('map ready', {'poly': poly, 'maps_key': os.environ['GOOGLE_MAPS_KEY']})
-
+@app.route('/map', methods=['GET', 'POST'])
+def map():
+    """Get the map for the given tweet"""
+    if request.form['tweetId']:
+        tweet_id = request.form['tweetId']
+        poly = determinate_tweet_location(tweet_id)
+        return render_template('map.html', poly=poly, maps_key=os.environ['GOOGLE_MAPS_KEY'])
 
 if __name__ == "__main__":
-    log.info(u'Starting server on port: {0}'.format(int(sys.argv[1])))
-    socketio.run(app, port=int(sys.argv[1]))
+    app.run()

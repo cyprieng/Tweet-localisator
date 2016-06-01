a0d928481240b1f21870e3cd69835077f81780a6
Cyprien G <cyprien.guillemot@gmail.com>
2016-06-01 19:23:57 +0200

    ENH: Support multi localization (Fix #5)

--- app.py
+++ app.py
@@ -9,19 +9,16 @@ from lib.tweet_locator import determinate_tweet_location
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    """Return the home page"""
-    return render_template('home.html')
-
-
-@app.route('/map', methods=['GET', 'POST'])
-def map():
-    """Get the map for the given tweet"""
-    if request.form['tweetId']:
-        tweet_id = request.form['tweetId']
-        polys = determinate_tweet_location(tweet_id)
+    """Return the home page or map if tweet_id is send"""
+    if request.form.get('tweetId'):
+        tweet_ids = request.form['tweetId'].split(',')
+        polys = []
+        for tweet_id in tweet_ids:
+            polys.append((determinate_tweet_location(tweet_id), tweet_id))
         return render_template('map.html', polys=polys, maps_key=os.environ['GOOGLE_MAPS_KEY'])
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run()
